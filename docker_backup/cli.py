7f172257f3533d0a9e0680e487c36cac9e3ccf4b
Reimund Klain <reimund.klain@condevtec.de>
2016-10-23 04:12:23 +0200

    requirements, switch to yaml based configuration

--- docker_backup/cli.py
+++ docker_backup/cli.py
@@ -15,44 +15,36 @@ from docker import client
 
 from pprint import pprint
 
+import yaml
+
 from . import dbs
 
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
-APPS = dict(
-    ubuntu=dict(
-        versions={
-        '14.04': [dict(cmd='ps aux')]
-        }
-    ),
-    postgres=dict(
-        versions={
-        '9.4': [
-            dict(
-                cmd='pg_dump -U postgres postgres',
-                output= True,
+CONFIG="""
 
-            ),
-            dict(
-                cmd='/bin/sh -c \"pg_dump -U postgres postgres > /var/lib/postgresql/data/db_backup_$(date +%u).sql\"'
-            )
-        ],
-        }
-    )
-)
+images:
+    ubuntu:
+      '14.04':
+        - ps aux
 
+    postgres:
+      '9.4':
+        - /bin/sh -c "pg_dump -U postgres postgres > /var/lib/postgresql/data/db_backup_$(date +%u).sql\"
+
+"""
 
 def main():
-
+    config = yaml.load(CONFIG)
     c = client.Client('unix://var/run/docker.sock')
     for container in c.containers():
         log.info("Handle container id: '{}' image: '{}'".format(container['Id'], container['Image']))
         log.debug(pprint(container))
-        app_backup(c, container)
+        app_backup(c, config, container)
 
 
-def app_backup(c, container):
+def app_backup(c, config, container):
     cid = container['Id']
     image = container['Image']
     name = container['Names'][0].replace('/', '')
@@ -60,30 +52,21 @@ def app_backup(c, container):
     if ':' in image:
         image, version = container['Image'].split(':')
 
-    app = APPS.get(image)
+    app = config.get('images').get(image)
     if not app:
         log.warning("No commoand found for image: '{}'".format(image))
         return
-    commands = app.get('versions').get(version)
+    commands = app.get(version)
     if not commands:
-        log.warning("No commoand found for version: '{}'".format(version))
+        log.warning("No command found for version: '{}'".format(version))
         return
 
     for command in commands:
-        cmd = command.get('cmd')
-        is_stream = command.get('is_stream', False)
+        cmd = command
         e = c.exec_create(cid, cmd)
-
-        fd = None
-        if is_stream:
-            fd = open('backup/{}.sql'.format(name), 'wb')
-        try:
-            for o in c.exec_start(e, stream=True):
-                if is_stream: fd.write(o)
-                for l in o.splitlines():
-                    log.debug(l)
-        finally:
-            if is_stream: fd.close()
+        for o in c.exec_start(e, stream=True):
+            for l in o.splitlines():
+                log.debug(l)
 
 if __name__ == '__main__':
     sys.exit(main())
