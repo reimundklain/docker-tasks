ecc6b5fb3129cc4c3a92de6522041b548d096460
Reimund Klain <reimund.klain@condevtec.de>
2016-10-23 04:00:05 +0200

    commands

--- docker_backup/cli.py
+++ docker_backup/cli.py
@@ -8,6 +8,7 @@ __copyright__ = 'Copyright 2016 Reimund Klain'
 
 import sys
 import logging
+import datetime as dt
 from collections import defaultdict
 
 from docker import client
@@ -20,14 +21,25 @@ log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 APPS = dict(
-    ubuntu={
-        '14.04': 'ps aux'
-    },
-    postgres={
-        '9.4': "pg_dump -U postgres postgres",
-        'latest': "pg_dump -U postgres postgres",
-    }
+    ubuntu=dict(
+        versions={
+        '14.04': [dict(cmd='ps aux')]
+        }
+    ),
+    postgres=dict(
+        versions={
+        '9.4': [
+            dict(
+                cmd='pg_dump -U postgres postgres',
+                output= True,
 
+            ),
+            dict(
+                cmd='/bin/sh -c \"pg_dump -U postgres postgres > /var/lib/postgresql/data/db_backup_$(date +%u).sql\"'
+            )
+        ],
+        }
+    )
 )
 
 
@@ -35,13 +47,15 @@ def main():
 
     c = client.Client('unix://var/run/docker.sock')
     for container in c.containers():
-        log.debug("Handle container id: '{}' image: '{}'".format(container['Id'], container['Image']))
+        log.info("Handle container id: '{}' image: '{}'".format(container['Id'], container['Image']))
+        log.debug(pprint(container))
         app_backup(c, container)
 
 
 def app_backup(c, container):
     cid = container['Id']
     image = container['Image']
+    name = container['Names'][0].replace('/', '')
     version = None
     if ':' in image:
         image, version = container['Image'].split(':')
@@ -50,17 +64,26 @@ def app_backup(c, container):
     if not app:
         log.warning("No commoand found for image: '{}'".format(image))
         return
-    command = app.get(version)
-    if not command:
+    commands = app.get('versions').get(version)
+    if not commands:
         log.warning("No commoand found for version: '{}'".format(version))
         return
 
-    e = c.exec_create(cid, command)
-    for o in c.exec_start(e, stream=True):
-        for l in o.splitlines():
-            print(l)
+    for command in commands:
+        cmd = command.get('cmd')
+        is_stream = command.get('is_stream', False)
+        e = c.exec_create(cid, cmd)
 
-
+        fd = None
+        if is_stream:
+            fd = open('backup/{}.sql'.format(name), 'wb')
+        try:
+            for o in c.exec_start(e, stream=True):
+                if is_stream: fd.write(o)
+                for l in o.splitlines():
+                    log.debug(l)
+        finally:
+            if is_stream: fd.close()
 
 if __name__ == '__main__':
     sys.exit(main())
