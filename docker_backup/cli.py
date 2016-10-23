24b19cb738337850b47d50871811df971d254399
Reimund Klain <reimund.klain@condevtec.de>
2016-10-23 02:50:37 +0200

    firt execution

---
+++ docker_backup/cli.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'docker-backup'
+__version__ = '0.1.0'
+__author__ = 'Reimund Klain'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2016 Reimund Klain'
+
+import sys
+import logging
+from collections import defaultdict
+
+from docker import client
+
+from pprint import pprint
+
+from . import dbs
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+APPS = dict(
+    ubuntu={
+        '14.04': 'ps aux'
+    }
+)
+
+
+def main():
+
+    c = client.Client('unix://var/run/docker.sock')
+    for container in c.containers():
+        log.debug("Handle container id: '{}' image: '{}'".format(container['Id'], container['Image']))
+        app_backup(c, container)
+
+
+def app_backup(c, container):
+    cid = container['Id']
+    name, version = container['Image'].split(':')
+    app = APPS.get(name)
+    if not app:
+        log.warning("No commoand found for image: '{}'".format(name))
+        return
+    command = app.get(version)
+    if not command:
+        log.warning("No commoand found for version: '{}'".format(version))
+        return
+
+    e = c.exec_create(cid, command)
+    for o in c.exec_start(e, stream=True):
+        for l in o.splitlines():
+            log.info(l)
+
+
+
+if __name__ == '__main__':
+    main()
+
+
--- docker_backup/dbs.py
+++ docker_backup/dbs.py
@@ -6,16 +6,3 @@ __author__ = 'Reimund Klain'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2016 Reimund Klain'
 
-import sys
-from docker import client
-
-def main():
-
-    c = client.Client('unix://var/run/docker.sock')
-    print(c.containers())
-
-
-if __name__ == '__main__':
-    main()
-
-
