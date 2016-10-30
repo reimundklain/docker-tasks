64efbf39a73954c8303ecabf539f1a3ace91eb5d
Reimund Klain <reimund.klain@condevtec.de>
2016-10-30 16:35:15 +0100

    remove old docker_backup

--- docker_backup/dbs.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__title__ = 'docker-backup'
-__version__ = '0.1.0'
-__author__ = 'Reimund Klain'
-__license__ = 'Apache 2.0'
-__copyright__ = 'Copyright 2016 Reimund Klain'
-
